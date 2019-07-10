75a94c04d3d4de244612ef93df99c0afc113b92d
GNG Deployment <email>
2019-07-10 10:52:10 -0700

    first

---
+++ loaner/web_app/frontend/src/components/checkout/checkout.ts
@@ -0,0 +1,53 @@
+// Copyright 2018 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {Component, OnInit} from '@angular/core';
+import {Title} from '@angular/platform-browser';
+import {ActivatedRoute, Router} from '@angular/router';
+
+import {LoaderView} from '../../../../../shared/components/loader';
+import {CONFIG} from '../../app.config';
+import {AuthService} from '../../services/auth';
+
+/**
+ * Component that renders the Authorization flow of the application.
+ */
+@Component({
+  preserveWhitespaces: true,
+  selector: 'loaner-checkout',
+  styleUrls: ['checkout.scss'],
+  templateUrl: 'checkout.ng.html',
+})
+export class Checkout extends LoaderView implements OnInit {
+  /** Title for the component. */
+  private readonly title = `Authorization - ${CONFIG.appName}`;
+  /** Url to be redirected after login. */
+  private returnUrl!: string;
+
+  constructor(
+      private readonly authService: AuthService,
+      private readonly route: ActivatedRoute,
+      private readonly router: Router,
+      private readonly titleService: Title,
+  ) {
+    super(true);
+
+  }
+
+  ngOnInit() {
+
+  }
+
+
+}
